df0f0c9b6f91dc96fa3d21cf7d0ab6d387ba5d83
Deepak Singh <deepak@Deepaks-MBP.bbrouter>
2022-09-14 17:23:28 +0530

    done

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -2,6 +2,8 @@ class RedirectsController < ApplicationController
 
   def index
     url = Url.find_by!(slug: params[:url_id])
+    url.count += 1
+    url.save
     puts "--------------"
     puts url.name
     redirect_to url.name, allow_other_host: true
