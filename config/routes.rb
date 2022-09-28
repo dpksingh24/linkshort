af8e8f024545044b28a478a160703da15c553545
Deepak Singh <deepak@Deepaks-MBP.bbrouter>
2022-09-28 16:18:43 +0530

    switching

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root "urls#index"
   get '/:url_id' => "redirects#redirect"
 
-  resources :users, only: [:index, :show]
+  # resources :users, only: [:index, :show]
+  get '/users' => "users#index"
+  get '/users/:id' => "users#show"
+
 
   resources :urls, only: [:create, :show, :index] do
     collection do
